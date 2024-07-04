d9066c929864f3c9f045a88729bac0c85d631df4
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2024-07-04 11:42:10 -0300

    refact: token expiration time setting

--- task-manager/app/auth/token.go
+++ task-manager/app/auth/token.go
@@ -2,6 +2,8 @@ package auth
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
@@ -15,15 +17,18 @@ type InputToken struct {
 
 // GenerateToken generates a JWT token
 func GenerateToken(ctx context.Context, input InputToken) (string, error) {
+	expirationTime := time.Now().Add(1 * time.Hour)
+
 	claims := jwt.MapClaims{
 		"public_id": input.PublicID,
 		"name":      input.Name,
 		"email":     input.Email,
+		"exp":       expirationTime.Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	tokenString, err := token.SignedString([]byte("secret"))
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 	if err != nil {
 		return "", err
 	}
